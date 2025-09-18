9dab38fb5409b7e6b1d8a9d4108262b1f6a9f8e6
mhdkharoub-rgb <mhdkharoub@gmail.com>
2025-09-18 20:13:35 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
 
-import Navbar from '@/components/Navbar'
-import Hero from '@/components/Hero'
-import Features from '@/components/Features'
-import Pricing from '@/components/Pricing'
-import Testimonials from '@/components/Testimonials'
-import FAQ from '@/components/FAQ'
-import Footer from '@/components/Footer'
+import Navbar from '../components/Navbar'
+import Hero from '../components/Hero'
+import Features from '../components/Features'
+import Pricing from '../components/Pricing'
+import Testimonials from '../components/Testimonials'
+import FAQ from '../components/FAQ'
+import Footer from '../components/Footer'
 
 export default function Home(){
   return (
