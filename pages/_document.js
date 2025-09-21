f5a126f98077b10e0205342c61e8d1cecc5f620f
mhdkharoub-rgb <mhdkharoub@gmail.com>
2025-09-21 15:24:51 +0300

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,20 @@
-import memberstackDOM from '@memberstack/dom';
-const memberstack = memberstackDOM.init({
-  publicKey: "pk_b11e99cdfec26904e3e0",
-});
+// pages/_document.js
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <script
+          src="https://api.memberstack.io/static/memberstack.js?custom"
+          data-memberstack-id="pk_b11e99cdfec26904e3e0"
+          async
+        ></script>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
