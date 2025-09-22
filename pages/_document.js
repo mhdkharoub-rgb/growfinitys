cb2fd2fb443e44a526cff5e1bd8fe0cde73d34cb
mhdkharoub-rgb <mhdkharoub@gmail.com>
2025-09-22 11:29:47 +0300

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,2 +1,20 @@
-<!-- Memberstack webflow package -->
-<script data-memberstack-app="app_cmfpuki7k01750wwsgi71cena" src="https://static.memberstack.com/scripts/v1/memberstack.js" type="text/javascript"></script>
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        {/* Memberstack MUST use your PUBLIC KEY (pk_...), not app_ */}
+        <script
+          src="https://api.memberstack.io/static/memberstack.js?custom"
+          data-memberstack-id="app_cmfpuki7k01750wwsgi71cena"
+          async
+        ></script>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
