53ad44f7ce38bc4c397b8e46c101e2a6c8edec58
mhdkharoub-rgb <mhdkharoub@gmail.com>
2025-09-22 09:49:26 +0300

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,13 @@
-// pages/_document.js
-import { Html, Head, Main, NextScript } from 'next/document';
+import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        {/* Memberstack MUST use your PUBLIC KEY (pk_...), not app_ */}
         <script
           src="https://api.memberstack.io/static/memberstack.js?custom"
-          data-memberstack-id="app_cmfpuki7k01750wwsgi71cena"
+          data-memberstack-id="pk_b11e99cdfec26904e3e0"
           async
         ></script>
       </Head>
@@ -16,5 +16,5 @@ export default function Document() {
         <NextScript />
       </body>
     </Html>
-  );
+  )
 }
