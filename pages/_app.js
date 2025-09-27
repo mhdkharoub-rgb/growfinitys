7a3a4dc0febfcd939d724f8f63a30b9cef9efddf
mhdkharoub-rgb <mhdkharoub@gmail.com>
2025-09-27 13:27:03 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,3 @@
-
-import '../styles/globals.css'
-export default function App({ Component, pageProps }) { return <Component {...pageProps} /> }
 import Head from "next/head"
 import "../styles/globals.css"
 
