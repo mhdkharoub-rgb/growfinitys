34272c90b595eda76afc901f81d735a3c51ded07
mhdkharoub-rgb <mhdkharoub@gmail.com>
2025-09-27 13:09:24 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,22 @@
 
 import '../styles/globals.css'
 export default function App({ Component, pageProps }) { return <Component {...pageProps} /> }
+import Head from "next/head"
+import "../styles/globals.css"
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <script
+          async
+          src="https://cdn.nas.io/sdk.js"
+          data-nasio-id="YOUR_NASIO_ID"
+        ></script>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
+}
+
+export default MyApp
