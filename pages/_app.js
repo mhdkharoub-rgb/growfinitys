bc3b977d3ad8c07ace307e2be3cacc526764b406
mhdkharoub-rgb <mhdkharoub@gmail.com>
2025-10-03 09:00:52 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import Head from "next/head"
 import "../styles/globals.css"
+import FloatingCTA from "../components/FloatingCTA"
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -8,10 +9,12 @@ function MyApp({ Component, pageProps }) {
         <script
           async
           src="https://cdn.nas.io/sdk.js"
-          data-nasio-id="YOUR_NASIO_ID"
+          data-nasio-id="YOUR_NASIO_ID" 
         ></script>
       </Head>
       <Component {...pageProps} />
+      {/* Floating CTA visible only on mobile */}
+      <FloatingCTA />
     </>
   )
 }
