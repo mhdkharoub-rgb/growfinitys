b0b900b01d6302a2cfe671d67155e937c645aebb
mhdkharoub-rgb <mhdkharoub@gmail.com>
2025-10-03 08:53:56 +0300

    Create FloatingCTA.js

---
+++ components/FloatingCTA.js
@@ -0,0 +1,11 @@
+// components/FloatingCTA.js
+export default function FloatingCTA() {
+  return (
+    <a
+      href="#pricing"
+      className="fixed bottom-6 right-6 bg-yellow-500 text-black font-bold py-3 px-6 rounded-full shadow-lg hover:bg-yellow-400 transition z-50 md:hidden"
+    >
+      Join Now 🚀
+    </a>
+  )
+}
