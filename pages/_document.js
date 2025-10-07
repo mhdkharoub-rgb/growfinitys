fb13a28d4c94250c1f00b8c87f47d45b3084524e
mhdkharoub-rgb <mhdkharoub@gmail.com>
2025-10-07 16:48:42 +0300

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -4,12 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        {/* Memberstack script */}
-        <script
-          src="https://api.memberstack.io/static/memberstack.js?custom"
-          data-memberstack-id="pk_b11e99cdfec26904e3e0"
-          async
-        ></script>
+
       </Head>
       <body>
         <Main />
