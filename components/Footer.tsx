1310dbdd7472612395881680cb45f2a9ddb8fb6a
mhdkharoub-rgb <mhdkharoub@gmail.com>
2025-10-19 12:44:00 +0300

    Add Footer component with copyright notice

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,1 +1,7 @@
-
+export default function Footer() {
+  return (
+    <footer style={{ borderTop: "1px solid #eee", padding: 12, textAlign: "center" }}>
+      <small>© {new Date().getFullYear()} Growfinitys</small>
+    </footer>
+  );
+}
