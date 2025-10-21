d8126108e8663b928ce43e733f70639cd1b037d4
mhdkharoub-rgb <mhdkharoub@gmail.com>
2025-10-21 10:31:04 +0300

    Update supabaseClient.ts

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,9 +1,9 @@
-"use client";
+'use client';
+import { createBrowserClient } from '@supabase/ssr';
 
-import { createBrowserClient } from "@supabase/ssr";
 
-export const createSupabaseClient = () =>
-  createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-  );
+export const supabaseClient = () =>
+createBrowserClient(
+process.env.NEXT_PUBLIC_SUPABASE_URL!,
+process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+);
