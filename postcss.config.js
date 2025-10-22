905504792d781b348fd269b5a2c9c3fec7da1a80
mhdkharoub-rgb <mhdkharoub@gmail.com>
2025-10-22 20:00:08 +0300

    Guard PostCSS config when Tailwind deps missing

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,23 @@
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-};
+const fs = require('fs');
+const path = require('path');
+
+function hasPackage(packageName) {
+  const packagePath = path.join(__dirname, 'node_modules', packageName);
+  return fs.existsSync(packagePath);
+}
+
+const plugins = {};
+
+if (hasPackage('tailwindcss')) {
+  plugins.tailwindcss = {};
+} else {
+  console.warn('Tailwind CSS is not installed; skipping Tailwind PostCSS plugin.');
+}
+
+if (hasPackage('autoprefixer')) {
+  plugins.autoprefixer = {};
+} else {
+  console.warn('Autoprefixer is not installed; skipping Autoprefixer PostCSS plugin.');
+}
+
+module.exports = { plugins };
