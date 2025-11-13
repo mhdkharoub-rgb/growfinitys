a4fcfd72f7e76de07bfb99b3696d03bda3878cf1
mhdkharoub-rgb <mhdkharoub@gmail.com>
2025-11-13 13:43:46 +0300

    Delete app/auth/logout directory

--- app/auth/logout/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
-import { cookies, headers } from "next/headers";
-
-export async function POST(request: Request) {
-  const supabase = createRouteHandlerClient({ cookies, headers });
-  await supabase.auth.signOut();
-  return NextResponse.redirect(new URL("/login", request.url));
-}
