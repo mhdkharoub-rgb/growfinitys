2f8a1b701609e175377bbe97711062495474d9fc
mhdkharoub-rgb <mhdkharoub@gmail.com>
2025-11-13 13:50:05 +0300

    Update route.ts

--- app/api/auth/logout/route.ts
+++ app/api/auth/logout/route.ts
@@ -1,9 +1,9 @@
 import { NextResponse } from "next/server";
 import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
-import { cookies, headers } from "next/headers";
+import { cookies } from "next/headers";
 
 export async function POST(request: Request) {
-  const supabase = createRouteHandlerClient({ cookies, headers });
+  const supabase = createRouteHandlerClient({ cookies });
   await supabase.auth.signOut();
   return NextResponse.redirect(new URL("/login", request.url));
 }
